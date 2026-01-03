a11c56a49235d17a9ff111030fab7c9ff485dd32
Nemanja-Jovicic <helmserbon@gmail.com>
2026-01-04 00:11:45 +0100

    category pagination

---
+++ models/category/filter.php
@@ -0,0 +1,18 @@
+<?php 
+header("Content-type:application/json");
+include '../response_functions.php';
+if($_SERVER["REQUEST_METHOD"] === "GET"){
+   $link = $_GET['link'];
+    try {
+        require_once '../../config/connection.php';
+        include '../functions.php';
+        normalResponse([
+            'data' => getAllCategories($link),
+            'pagination' => $categoryPagination()
+        ]);
+    } catch (PDOException $th) {
+        intervalServerError($th->getMessage());
+    }
+}else{
+    notFound("Stranica nije pronadjena!");
+}
