37ac8772326836e07e26a9e04a75f42b4d89c5af
Nemanja-Jovicic <helmserbon@gmail.com>
2026-01-04 22:49:21 +0100

    Store functionality

--- models/tour/store.php
+++ models/tour/store.php
@@ -3,17 +3,14 @@ header("Content-type:application/json");
 include '../response_functions.php';
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
-    // public function create(Request $request, CreateValidatin $valition){
-        // Tour($validation);
-    //}
     $name = $_POST['tour_name'];
-    $description = ''; // quill
+    $description = $_POST['tour_description'];
     $price = $_POST['tour_price'];
     $duration = $_POST['tour_duration'];
     $categories = $_POST['categories'];
     $banner = $_FILES['banner'];
     $tour_tags = $_POST['tour_tags'];
-
+    
     include '../validation.php';
     $validation = tourFormValidation(
         $name,
@@ -24,26 +21,30 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $tour_tags,
         $banner
     );
+  
+    
+
     if (count($validation) > 0) {
         validationError($validation);
     }else{
-        try {
-            require_once '../../config/connection.php';
-            include '../functions.php';
+         try {
+             require_once '../../config/connection.php';
+             include '../functions.php';
 
-            if($checkTourName($name)){
-                conflictError("Tura sa tim nazivom vec postoji!");
-            }else{
-                $banner_name = moveBanner($banner);
-                $tags  = checkTags($tour_tags);
-                storeNewTour($name, $description, $price, $duration,
-                    $categories, $tags, $banner);
-                successfullResponseCode(['message' => 'Nova tura je kreirana!']);
-            }
-        } catch (PDOException $th) {
-            intervalServerError($th->getMessage());
+             if($checkTourName($name)){
+                 conflictError("Tura sa tim nazivom vec postoji!");
+             }else{
+                 $banner_name = moveBanner($banner);
+                 $tags  = checkTags($tour_tags);
+                 storeNewTour($name, $description, $price, $duration,
+                     $categories, $tags, $banner_name);
+                 successfullResponseCode(['message' => 'Nova tura je kreirana!', 
+                        'data' => getAllTours()]);
+             }
+         } catch (PDOException $th) {
+             intervalServerError($th->getMessage());
         }
-    }
+     }
 } else {
     notFound("Stranica nije pronadjena!");
 }
