13cf90d0f2e500bb1601a3fcbb26200d7c418256
Nemanja-Jovicic <helmserbon@gmail.com>
2026-01-09 00:00:29 +0100

    functionality to get one date

---
+++ models/date/edit.php
@@ -0,0 +1,17 @@
+<?php 
+header('Content-type:application/json');
+include '../response_functions.php';
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+    $id  = $_GET['id'];
+    
+    try {
+        require_once '../../config/connection.php';
+        include '../functions.php';
+        normalResponse(['data' => $getOneTourDateData($id)]);
+    } catch (PDOException $th) {
+        intervalServerError($th->getMessage());
+    }
+}
+else{
+    notFound('Stranica nije pronadjena');
+}
