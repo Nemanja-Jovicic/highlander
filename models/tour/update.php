5f4d30c2e2ab44f1e0bac988014b504609deeaf6
Nemanja-Jovicic <helmserbon@gmail.com>
2026-01-18 22:17:46 +0100

    fixed errors

--- models/tour/update.php
+++ models/tour/update.php
@@ -2,15 +2,16 @@
 header("Content-type:application/json");
 include '../response_functions.php';
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $id = $_POST['tour_id'];
+    $id = $_POST['id'];
     $name = $_POST['tour_name'];
-    $description = ''; // quill
+    $description = $_POST['tour_description'];
     $price = $_POST['tour_price'];
     $duration = $_POST['tour_duration'];
     $categories = $_POST['categories'];
     $banner = isset($_FILES['banner']) ? $_FILES['banner'] : '';
     $tour_tags = $_POST['tour_tags'];
-
+    $cover = $_POST['banner_cover'];
+    
     include '../validation.php';
     $validation = tourFormValidation(
         $name,
@@ -29,17 +30,19 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             require_once '../../config/connection.php';
             include '../functions.php';
             $checkTourName = $checkTourName($name);
-            if ($checkTourName && $checkTourName->name === $name && $checkTourName->id !== $id) {
+            if ($checkTourName && $checkTourName->name === $name && $checkTourName->id !== (int)$id) {
                 conflictError("Tura sa tim nazivom vec postoji");
             } else {
                 $banner_name = "";
                 if ($banner !== "") {
-                    unlinkBanner($banner);
+                    unlinkBanner($cover);
                     $banner_name = moveBanner($banner);
                 }
-                updateBanner($id, $name, $duration,
-                    $price, $duration, $categories, $tour_tags, banner: $banner_name);
-            normalResponse(['message' => 'Tura je uspesno izmenjena', 'data' => $getOneFullRow('tours', $id)]);
+                updateTour($id, $name, $description, $price, $duration, $banner_name, $tour_tags, $categories);
+                normalResponse([
+                    'message' => 'Tura je uspesno izmenjena',
+                    'data' => $getOneTourFullRow($id)
+                ]);
             }
         } catch (PDOException $th) {
             intervalServerError($th->getMessage());
