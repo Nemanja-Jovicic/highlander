d74715f30781599f7a58ebb5d4f11d03e1870243
Nemanja-Jovicic <helmserbon@gmail.com>
2026-01-18 22:20:47 +0100

    contact page

--- includes/pages/user/contact.php
+++ includes/pages/user/contact.php
@@ -1,10 +1,17 @@
+<?php
+
+if(isset($_SESSION['user']) && $_SESSION['user']->role_id === 1){
+    header("Location: admin.php?page=greske&code=403");
+}
+?>
 <main class="container">
     <div class="row mt-5">
         <div id="message"></div>
     </div>
     <div class="row mt-2">
-        <div class="col-lg-6 mx-auto">
-            <form action="#">
+        <div class="col-lg-4 mx-auto">
+            <form action="#" method="post">
+                <h1 class="mb-3 fs-3">Kontaktirajte nas</h1>
                 <div class="mb-2">
                     <label for="first_name" class="mb-1">Ime</label>
                     <input type="text" name="first_name" id="first_name" class="form-control mb-1">
@@ -21,13 +28,13 @@
                     <div id="email_error"></div>
                 </div>
                 <div class="mb-2">
-                    <label for="message" class="mb-1">Poruka</label>
-                    <textarea name="message" id="message" class="form-control mb-1"></textarea>
+                    <label for="message_field" class="mb-1">Poruka</label>
+                    <textarea name="message_field" id="message_field" class="form-control mb-1"></textarea>
                     <div id="message_error"></div>
                 </div>
-                <div class="d-flex justify-content-end">
+               
                     <button class="btn btn-sm btn-primary"type='button' id="btnContactUs">Posalji</button>
-                </div>
+            
             </form>
         </div>
     </div>
