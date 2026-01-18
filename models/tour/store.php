3ca82e2a43371665e87cae5dd5ac32d6c039712e
Nemanja-Jovicic <helmserbon@gmail.com>
2026-01-18 22:18:36 +0100

    remove unnesecery things

--- models/tour/store.php
+++ models/tour/store.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $categories = $_POST['categories'];
     $banner = $_FILES['banner'];
     $tour_tags = $_POST['tour_tags'];
-    
+
     include '../validation.php';
     $validation = tourFormValidation(
         $name,
@@ -21,30 +21,30 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $tour_tags,
         $banner
     );
-  
-    
+
+
 
     if (count($validation) > 0) {
         validationError($validation);
-    }else{
-         try {
-             require_once '../../config/connection.php';
-             include '../functions.php';
+    } else {
+        try {
+            require_once '../../config/connection.php';
+            include '../functions.php';
 
-             if($checkTourName($name)){
-                 conflictError("Tura sa tim nazivom vec postoji!");
-             }else{
-                 $banner_name = moveBanner($banner);
-                 $tags  = checkTags($tour_tags);
-                 storeNewTour($name, $description, $price, $duration,
-                     $categories, $tags, $banner_name);
-                 successfullResponseCode(['message' => 'Nova tura je kreirana!', 
-                        'data' => getAllTours()]);
-             }
-         } catch (PDOException $th) {
-             intervalServerError($th->getMessage());
+            if ($checkTourName($name)) {
+                conflictError("Tura sa tim nazivom vec postoji!");
+            } else {
+                $banner_name = moveBanner($banner);
+                storeNewTour($name, $description, $duration, $price, $banner_name, $categories, $tour_tags);
+                successfullResponseCode([
+                    'message' => "Tura je uspesno kreirana!",
+                    'data' => getAllTours()
+                ]);
+            }
+        } catch (PDOException $th) {
+            intervalServerError($th->getMessage());
         }
-     }
+    }
 } else {
     notFound("Stranica nije pronadjena!");
 }
